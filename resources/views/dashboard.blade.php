81e284dcd9e4601dc0ea6134d2b4789b046ae1ee
Junzel Dela Torre <107986856+NekoMeowCat@users.noreply.github.com>
2023-12-05 15:58:00 +0800

    dashboard with sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,7 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+    <div class="min-h-screen max-w-screen overflow-hidden flex">
+        <x-sidebar />
+        <div class="m-4 bg-red-400 min-h-screen w-full">
         </div>
     </div>
 </x-app-layout>
